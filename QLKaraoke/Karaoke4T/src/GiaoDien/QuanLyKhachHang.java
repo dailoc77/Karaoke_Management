daa4cbea62b16c52efa16657e81c0494fbc2ceae
Thạch Thiên <maihothienthach12a1thpt@gmail.com>
2023-10-29 12:17:32 +0700

    giao dien quan ly khách hàng

---
+++ QLKaraoke/Karaoke4T/src/GiaoDien/QuanLyKhachHang.java
@@ -0,0 +1,198 @@
+package GiaoDien;
+
+import java.awt.Color;
+import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+import javax.swing.border.EmptyBorder;
+import javax.swing.ImageIcon;
+import javax.swing.JTextField;
+
+public class QuanLyKhachHang extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					QuanLyKhachHang frame = new QuanLyKhachHang();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+		
+		
+		
+		
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	
+	
+	
+	public QuanLyKhachHang() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 1175, 650);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		
+		
+		JPanel datphong = new JPanel();
+		datphong.setBounds(0, 70, 232, 80);
+//		datphong.addMouseListener(new MouseAdapter() {
+//			@Override
+//			public void mouseClicked(MouseEvent e) {
+//
+//			}
+//		});
+		
+		datphong.setBorder(null);
+		datphong.setBackground(new Color(0, 0, 0, 150));
+		contentPane.add(datphong);
+		datphong.setLayout(null);
+		
+		JLabel lbldatphong = new JLabel("Đặt Phòng");
+		lbldatphong.setForeground(Color.WHITE);
+		lbldatphong.setFont(new Font("Tahoma", Font.BOLD, 20));
+		lbldatphong.setHorizontalAlignment(SwingConstants.CENTER);
+		lbldatphong.setBounds(0, 0, 232, 80);
+		datphong.add(lbldatphong);
+		
+		JPanel thuephong = new JPanel();
+		thuephong.setBounds(229, 70, 232, 80);
+		thuephong.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+			}
+		});
+		thuephong.setLayout(null);
+		thuephong.setBorder(null);
+		thuephong.setBackground(new Color(0, 0, 0, 150));
+		contentPane.add(thuephong);
+		
+		JLabel lblthuephong = new JLabel("Thuê Phòng");
+		lblthuephong.setHorizontalAlignment(SwingConstants.CENTER);
+		lblthuephong.setForeground(Color.WHITE);
+		lblthuephong.setFont(new Font("Tahoma", Font.BOLD, 20));
+		lblthuephong.setBounds(0, 0, 232, 80);
+		thuephong.add(lblthuephong);
+		
+		JPanel datdichvu = new JPanel();
+		datdichvu.setBounds(462, 70, 232, 80);
+		datdichvu.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+			}
+		});
+		datdichvu.setLayout(null);
+		datdichvu.setBorder(null);
+		datdichvu.setBackground(new Color(0, 0, 0, 150));
+		contentPane.add(datdichvu);
+		
+		JLabel lbltdatdichvu = new JLabel("Đặt Dịch Vụ");
+		lbltdatdichvu.setHorizontalAlignment(SwingConstants.CENTER);
+		lbltdatdichvu.setForeground(Color.WHITE);
+		lbltdatdichvu.setFont(new Font("Tahoma", Font.BOLD, 20));
+		lbltdatdichvu.setBounds(0, 0, 232, 80);
+		datdichvu.add(lbltdatdichvu);
+		
+		JPanel khachhang = new JPanel();
+		khachhang.setBounds(695, 70, 232, 80);
+		khachhang.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+			}
+		});
+		khachhang.setLayout(null);
+		khachhang.setBorder(null);
+		khachhang.setBackground(new Color(0, 0, 0, 150));
+		contentPane.add(khachhang);
+		
+		JLabel lbltkhachhang = new JLabel("Khách Hàng");
+		lbltkhachhang.setHorizontalAlignment(SwingConstants.CENTER);
+		lbltkhachhang.setForeground(Color.WHITE);
+		lbltkhachhang.setFont(new Font("Tahoma", Font.BOLD, 20));
+		lbltkhachhang.setBounds(0, 0, 232, 80);
+		khachhang.add(lbltkhachhang);
+		
+		JPanel thongke = new JPanel();
+		thongke.setBounds(928, 70, 233, 80);
+		thongke.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+			}
+		});
+		thongke.setLayout(null);
+		thongke.setBorder(null);
+		thongke.setBackground(new Color(0, 0, 0, 150));
+		contentPane.add(thongke);
+		
+		JLabel lblthongke = new JLabel("Thống Kê");
+		lblthongke.setHorizontalAlignment(SwingConstants.CENTER);
+		lblthongke.setForeground(Color.WHITE);
+		
+		
+		lblthongke.setFont(new Font("Tahoma", Font.BOLD, 20));
+		lblthongke.setBounds(0, 0, 232, 80);
+		thongke.add(lblthongke);
+		
+		JPanel panel = new JPanel();
+		panel.setBounds(0, 149, 1161, 128);
+		panel.setBackground(new Color(255, 255, 255,150));
+		contentPane.add(panel);
+		panel.setLayout(null);
+		
+		JLabel lblNewLabel_1 = new JLabel("Nhập họ và tên");
+		lblNewLabel_1.setBounds(62, 28, 92, 14);
+		panel.add(lblNewLabel_1);
+		
+		JLabel lblNewLabel_1_1 = new JLabel("Mã khách hàng");
+		lblNewLabel_1_1.setBounds(62, 64, 92, 14);
+		panel.add(lblNewLabel_1_1);
+		
+		JLabel lblNewLabel_1_2 = new JLabel("Số điện thoại");
+		lblNewLabel_1_2.setBounds(303, 28, 92, 14);
+		panel.add(lblNewLabel_1_2);
+		
+		JLabel lblNewLabel_1_3 = new JLabel("Số CMND");
+		lblNewLabel_1_3.setBounds(303, 64, 92, 14);
+		panel.add(lblNewLabel_1_3);
+		
+		JLabel lblNewLabel_1_4 = new JLabel("Địa chỉ");
+		lblNewLabel_1_4.setBounds(633, 76, 92, 14);
+		panel.add(lblNewLabel_1_4);
+		
+		JLabel lblNewLabel_1_5 = new JLabel("Nam");
+		lblNewLabel_1_5.setBounds(574, 28, 92, 14);
+		panel.add(lblNewLabel_1_5);
+		
+		JLabel lblNewLabel_1_6 = new JLabel("Nữ");
+		lblNewLabel_1_6.setBounds(698, 28, 92, 14);
+		panel.add(lblNewLabel_1_6);
+		
+		JLabel lblNewLabel = new JLabel("");
+		lblNewLabel.setBounds(0, 0, 1161, 613);
+		contentPane.add(lblNewLabel);
+		lblNewLabel.setBackground(new Color(240, 240, 240));
+		lblNewLabel.setForeground(new Color(255, 0, 0));
+		lblNewLabel.setIcon(new ImageIcon("D:\\git_duanlon\\PTUD_2023_Nhom07_DHKTPM17ATT\\QLKaraoke\\Karaoke4T\\src\\Imgs\\370.png"));
+	}
+}
