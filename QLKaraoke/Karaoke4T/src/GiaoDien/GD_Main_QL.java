629a091af17c235104c9bda3dc3f3dcd6d7cf215
maiquoctruong <nguyenthanhtung0900@gmail.com>
2023-10-29 13:55:18 +0700

    add giao quan ly

---
+++ QLKaraoke/Karaoke4T/src/GiaoDien/GD_Main_QL.java
@@ -0,0 +1,216 @@
+package GiaoDien;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+//import java.util.GregorianCalendar;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import java.awt.Color;
+import java.awt.EventQueue;
+
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+import java.awt.Font;
+import javax.swing.SwingConstants;
+import javax.swing.Timer;
+
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.text.SimpleDateFormat;
+import java.time.Clock;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+public class GD_Main_QL extends JFrame implements ActionListener{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+	private JLabel lblClock;
+	private Timer timer;
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					GD_Main_QL frame = new GD_Main_QL();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+	/**
+	 * Create the frame.
+	 */
+	public GD_Main_QL() {
+		setBackground(Color.WHITE);
+		setTitle("Giao Diện Quản Lý");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setLocationRelativeTo(null);
+		setBounds(100, 100, 1175, 650);
+		contentPane = new JPanel();
+		contentPane.setBorder(null);
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JPanel box_clock = new JPanel();
+        box_clock.setBackground(new Color(255, 255, 255));
+        box_clock.setBounds(34, 10, 260, 50);
+        contentPane.add(box_clock);
+        box_clock.setLayout(null);
+
+        lblClock = new JLabel();
+        lblClock.setHorizontalAlignment(SwingConstants.CENTER);
+        lblClock.setFont(new Font("Tahoma", Font.PLAIN, 20));
+        lblClock.setBounds(10, 0, 240, 50);
+        lblClock.setForeground(Color.BLACK);
+        box_clock.add(lblClock);
+
+        timer = new Timer(0, this);
+        timer.start();
+		
+		JPanel datphong = new JPanel();
+		datphong.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+
+			}
+		});
+		datphong.setBorder(null);
+		datphong.setBackground(new Color(0, 0, 0, 150));
+		datphong.setBounds(0, 70, 232, 80);
+		contentPane.add(datphong);
+		datphong.setLayout(null);
+		
+		JLabel lbldatphong = new JLabel("Phòng Hát");
+		lbldatphong.setForeground(Color.WHITE);
+		lbldatphong.setFont(new Font("Tahoma", Font.BOLD, 20));
+		lbldatphong.setHorizontalAlignment(SwingConstants.CENTER);
+		lbldatphong.setBounds(0, 0, 232, 80);
+		datphong.add(lbldatphong);
+		
+		JPanel thuephong = new JPanel();
+		thuephong.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+			}
+		});
+		thuephong.setLayout(null);
+		thuephong.setBorder(null);
+		thuephong.setBackground(new Color(0, 0, 0, 150));
+		thuephong.setBounds(229, 70, 232, 80);
+		contentPane.add(thuephong);
+		
+		JLabel lblthuephong = new JLabel("Dịch Vụ");
+		lblthuephong.setHorizontalAlignment(SwingConstants.CENTER);
+		lblthuephong.setForeground(Color.WHITE);
+		lblthuephong.setFont(new Font("Tahoma", Font.BOLD, 20));
+		lblthuephong.setBounds(0, 0, 232, 80);
+		thuephong.add(lblthuephong);
+		
+		JPanel datdichvu = new JPanel();
+		datdichvu.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+			}
+		});
+		datdichvu.setLayout(null);
+		datdichvu.setBorder(null);
+		datdichvu.setBackground(new Color(0, 0, 0, 150));
+		datdichvu.setBounds(462, 70, 232, 80);
+		contentPane.add(datdichvu);
+		
+		JLabel lbltdatdichvu = new JLabel("Nhân Viên");
+		lbltdatdichvu.setHorizontalAlignment(SwingConstants.CENTER);
+		lbltdatdichvu.setForeground(Color.WHITE);
+		lbltdatdichvu.setFont(new Font("Tahoma", Font.BOLD, 20));
+		lbltdatdichvu.setBounds(0, 0, 232, 80);
+		datdichvu.add(lbltdatdichvu);
+		
+		JPanel khachhang = new JPanel();
+		khachhang.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+			}
+		});
+		khachhang.setLayout(null);
+		khachhang.setBorder(null);
+		khachhang.setBackground(new Color(0, 0, 0, 150));
+		khachhang.setBounds(695, 70, 232, 80);
+		contentPane.add(khachhang);
+		
+		JLabel lbltkhachhang = new JLabel("Tài Khoản");
+		lbltkhachhang.setHorizontalAlignment(SwingConstants.CENTER);
+		lbltkhachhang.setForeground(Color.WHITE);
+		lbltkhachhang.setFont(new Font("Tahoma", Font.BOLD, 20));
+		lbltkhachhang.setBounds(0, 0, 232, 80);
+		khachhang.add(lbltkhachhang);
+		
+		JPanel thongke = new JPanel();
+		thongke.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+			}
+		});
+		thongke.setLayout(null);
+		thongke.setBorder(null);
+		thongke.setBackground(new Color(0, 0, 0, 150));
+		thongke.setBounds(928, 70, 233, 80);
+		contentPane.add(thongke);
+		
+		JLabel lblthongke = new JLabel("Thống Kê");
+		lblthongke.setHorizontalAlignment(SwingConstants.CENTER);
+		lblthongke.setForeground(Color.WHITE);
+		lblthongke.setFont(new Font("Tahoma", Font.BOLD, 20));
+		lblthongke.setBounds(0, 0, 232, 80);
+		thongke.add(lblthongke);
+		
+		JLabel lblNewLabel = new JLabel("");
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setIcon(new ImageIcon(GD_Main_NV.class.getResource("/Imgs/370.png")));
+		lblNewLabel.setBounds(-95, -176, 1333, 957);
+		contentPane.add(lblNewLabel);
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() == timer) {
+            // Cập nhật thời gian
+            updateClock();
+        }
+	}
+    private void updateClock() {
+        Calendar cal = Calendar.getInstance();
+        int hour = cal.get(Calendar.HOUR_OF_DAY);
+        int minute = cal.get(Calendar.MINUTE);
+        int second = cal.get(Calendar.SECOND);
+        int day = cal.get(Calendar.DAY_OF_MONTH);
+        int month = cal.get(Calendar.MONTH)+1;
+        int year = cal.get(Calendar.YEAR);
+        
+        String ampm;
+        if (hour >= 12) {
+            ampm = "PM";
+            if (hour > 24) {
+                hour -= 12;
+            }
+        } else {
+            ampm = "AM";
+            if (hour == 0) {
+                hour = 12;
+            }
+        }
+        
+        String time = String.format("%02d:%02d:%02d %s  %04d/%02d/%02d", hour, minute, second, ampm, year, month, day);
+        lblClock.setText(time);
+    }
+}
